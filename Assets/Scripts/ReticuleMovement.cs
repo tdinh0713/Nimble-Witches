b9ad8c267ab548a34f02de27e776ca753fb4d333
JDBar <jdbar1215@gmail.com>
2016-09-24 15:03:46 -0700

    More spellcaster stuff. It's a hodge podge

---
+++ Assets/Scripts/ReticuleMovement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReticuleMovement : MonoBehaviour {
+
+    public Transform mySpellcaster;
+    public Sprite[] mySprites;
+
+    private Transform myTransform;
+    private SpriteRenderer mySpriteRenderer;
+    private int mode;
+    private float aimMaxRadius;
+    private float aimRotation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myTransform = GetComponent<Transform>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 aimInput = new Vector2(Input.GetAxis("Spell Horizontal"), Input.GetAxis("Spell Vertical"));
+
+        switch (mode)
+        {
+            case 0: // NO RETICULE
+                break;
+
+            case 1: // STRAIGHT LINE
+                    float degrees = Vector2.Angle(new Vector2(0, 1), aimInput.normalized);
+                    aimRotation = degrees;
+                break;
+
+            case 2: // CIRCLE
+                break;
+        }
+	}
+
+    public void SetMode (int mode)
+    {
+        if (this.mode != mode)
+        {
+            this.mode = mode;
+            mySpriteRenderer.sprite = mySprites[mode];
+        }
+    }
+
+    public void SetAimMaxRadius (float radius)
+    {
+        aimMaxRadius = radius;
+    }
+
+    public void SetAimRotation (float rotation)
+    {
+        aimRotation = rotation;
+    }
+
+    public int GetMode ()
+    {
+        return mode;
+    }
+
+    public float GetAimRotation ()
+    {
+        return aimRotation;
+    }
+}
--- Assets/Scripts/Spellcaster.cs
+++ Assets/Scripts/Spellcaster.cs
@@ -3,33 +3,101 @@ using System.Collections;
 
 public class Spellcaster : MonoBehaviour {
 
-    public float inputWindow;
     public GameObject[] spells;
 
     private float inputTime;
     private bool expectingSecondInput;
 
+    // Spell elements are powers of 2 for bitmasking.
     private const int NUMBER_OF_SPELLS = 4;
+    private const int FIRE = 1;
+    private const int EARTH = 2;
+    private const int WATER = 4;
+    private const int AIR = 8;
     private int spell1, spell2;
 
-
-
-    private
+    private Queue spellQueue;
 
 	// Use this for initialization
 	void Start ()
     {
-        
+        spellQueue = new Queue();
 	}
 
     // Update is called once per frame
     void Update()
     {
+        // Check all spell element inputs, enqueue the input.
+        for (int i = 1; i <= NUMBER_OF_SPELLS; i++)
+        {
+            bool pressedButton = Input.GetButtonDown("Spell " + i.ToString());
+            if (pressedButton == true)
+            {
+                spellQueue.Enqueue(i);
+                if (spellQueue.Count > 2)
+                    spellQueue.Dequeue();
+            }
+        }
 
+        // Cancel Spell
+        if (Input.GetButtonDown("Cancel Spell"))
+        {
+            spellQueue.Clear();
+        }
+
+        // Cast Spell
+        if (Input.GetButtonDown("Cast Spell"))
+        {
+            CastSpell((int)spellQueue.Dequeue(), (int)spellQueue.Dequeue());
+        }
     }
 
     void CastSpell (int spell1, int spell2)
     {
-        
+        int spell;
+        if (spell1 == spell2)
+            spell = spell1;
+        else
+            spell = spell1 + spell2;
+
+        switch (spell)
+        {
+            case 1: // FIRE
+                // instantiate spells[0]
+                break;
+
+            case 2: // EARTH
+                break;
+
+            case 3: // FIRE + EARTH
+                break;
+
+            case 4: // WATER
+                break;
+
+            case 5: // FIRE + WATER
+                break;
+
+            case 6: // EARTH + WATER
+                break;
+
+            case 7: // INVALID
+                break;
+
+            case 8: // AIR
+                break;
+
+            case 9: // FIRE + AIR
+                break;
+
+            case 10: // EARTH + AIR
+                break;
+
+            case 11: // INVALID
+                break;
+
+            case 12: // WATER + AIR
+                break;
+        }
     }
 }
