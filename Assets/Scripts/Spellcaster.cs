dd247c684f1fae045971bb531961b44cef0c1d75
JDBar <jdbar1215@gmail.com>
2016-09-23 20:24:44 -0700

    Added spellcasting component
    Nothing in it yet

---
+++ Assets/Scripts/Spellcaster.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spellcaster : MonoBehaviour {
+
+    public float inputWindow;
+    public GameObject[] spells;
+
+    private float inputTime;
+    private bool expectingSecondInput;
+
+    private const int NUMBER_OF_SPELLS = 4;
+    private int spell1, spell2;
+
+
+
+    private
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CastSpell (int spell1, int spell2)
+    {
+        
+    }
+}
